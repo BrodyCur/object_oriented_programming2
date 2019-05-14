8cc2cb160362e74016df787d53fcce05c5b3567f
Brody Currie <brodycurrie@gmail.com>
2019-05-14 13:55:18 -0400

    finished exercise 2 in reinforcements

--- reinforcement.py
+++ reinforcement.py
@@ -1,11 +1,24 @@
-def odd_list(numlist):
-    for num in numlist:
-        if num % 2 != 0:
-            print(num)
-        else:
-            continue
+# def odd_list(numlist):
+#     for num in numlist:
+#         if num % 2 != 0:
+#             print(num)
+#         else:
+#             continue
 
-list1 = list(range(1, 31))
+# list1 = list(range(1, 31))
 
-odd_list(list1)
+# odd_list(list1)
 
+
+def greeting(name):
+    names = ['Brody', 'Kayla', 'Tibby']
+    if name == name in names:
+        print(f'Hello {name}!')
+    else:
+        print('Who goes there!?')
+
+greeting('Brody')
+greeting('Kayla')
+greeting('Tibby')
+greeting('Josh')
+greeting('Jasmin')
